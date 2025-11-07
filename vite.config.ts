0f672e0cfbac7d369685daf271fd43b2424fa5f5
diaorui <diaorui1987@gmail.com>
2025-11-06 16:03:03 -0800

    update path

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/ruidiao_website/',
+      base: '/',
       build: {
         outDir: 'docs',
+        emptyOutDir: false,
       },
       server: {
         port: 3000,
